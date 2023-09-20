800bb42806716bddc30ddc71489fd8ec6582347c
Elisey Ilin <e.ilin@powerinsight.io>
2023-09-20 15:24:27 +0300

    change name route2

--- config/provider.go
+++ config/provider.go
@@ -10,12 +10,12 @@ import (
 
 	ujconfig "github.com/upbound/upjet/pkg/config"
 
-	"github.com/upbound/upjet-provider-template/config/null"
+	"github.com/Jiraiya106/provider-vcd/config/vcd_network_routed_v2"
 )
 
 const (
-	resourcePrefix = "template"
-	modulePath     = "github.com/upbound/upjet-provider-template"
+	resourcePrefix = "vcd"
+	modulePath     = "github.com/Jiraiya106/provider-vcd"
 )
 
 //go:embed schema.json
@@ -27,7 +27,7 @@ var providerMetadata string
 // GetProvider returns provider configuration
 func GetProvider() *ujconfig.Provider {
 	pc := ujconfig.NewProvider([]byte(providerSchema), resourcePrefix, modulePath, []byte(providerMetadata),
-		ujconfig.WithRootGroup("template.upbound.io"),
+		ujconfig.WithRootGroup("crossplane.io"),
 		ujconfig.WithIncludeList(ExternalNameConfigured()),
 		ujconfig.WithFeaturesPackage("internal/features"),
 		ujconfig.WithDefaultResourceOptions(
@@ -36,7 +36,7 @@ func GetProvider() *ujconfig.Provider {
 
 	for _, configure := range []func(provider *ujconfig.Provider){
 		// add custom config functions
-		null.Configure,
+		vcd-network-routed-v2.Configure,
 	} {
 		configure(pc)
 	}
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/upbound/upjet-provider-template
+module github.com/Jiraiya106/provider-vcd
 
 go 1.19
 
